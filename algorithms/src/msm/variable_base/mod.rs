5e20ce793acd93336d8101566755bca0ffdfebda
IronC <apehole@163.com>
2022-07-27 14:46:16 +0800

    fix undeclared crate

--- algorithms/src/msm/variable_base/mod.rs
+++ algorithms/src/msm/variable_base/mod.rs
@@ -33,6 +33,7 @@ use core::sync::atomic::{AtomicBool, Ordering};
 
 #[cfg(all(feature = "cuda", target_arch = "x86_64"))]
 use std::env;
+#[cfg(all(feature = "cuda", target_arch = "x86_64"))]
 use rust_gpu_tools::GPUError;
 
 #[cfg(all(feature = "cuda", target_arch = "x86_64"))]
